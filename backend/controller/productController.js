1ac3f655c0acc461ca961c6df83e5225f1e97e5c
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2022-10-10 05:34:39 +0530

    just compleate the product routes, such as get all products, create product ,update product ,delete product,get single product details etc. And all routes are imported on the productRoute.js file.Next is error handling.

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -12,9 +12,70 @@ exports.createProduct=async(req,res,next)=>{
 
 }
 
+//get all products
+exports.getAllProducts=async(req,res)=>{
 
-exports.getAllProducts=(req,res)=>{
+    const products=await Product.find();
 
-    res.status(200).json({message:"route is working fine"})
+    res.status(200).json({
+        success:true,
+        products
 
+    })
+
+}
+// get single product details
+exports.getProductDetails=async(req,res,next)=>{
+
+    const product=await Product.findById(req.params.id);
+
+if(!product){
+    return res.status(500).json({
+        success:false,
+        message:"Product not found"
+    })
+}
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+}
+
+//Update product
+
+exports. updateProduct=async (req,res,next)=>{
+
+    const product=await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+updated_prouduct = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true,
+            runValidators:true,
+            useFindAndModify:false});
+
+            res.status(200).json({
+                success:true,
+                updated_prouduct
+            })
+}
+//delete product 
+exports.deleteProduct=async(req,res,next)=>{
+    const product=await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+
+    }
+     await product.remove();
+     res.status(200).json({
+        success:true,
+        message:"Product deleted successfully"
+     })
 }
